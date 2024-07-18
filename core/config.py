64a13ba6d8d3ab8687e0dc95b26f006c1b32e389
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-07-19 03:02:18 +0700

    created db_helper

--- core/config.py
+++ core/config.py
@@ -1,22 +1,30 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
-    # TODO: for now debug is unused
-    debug: bool = False
+    # TODO: unused now
+    debug: bool = True
 
 
 class APIConfig(BaseModel):
     prefix: str = "/api"
 
 
+class DBConfig(BaseModel):
+    url: PostgresDsn
+    debug: bool = True
+    pool_size: int = 5
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: APIConfig = APIConfig()
-    # db_url: str
+    db: DBConfig
 
 
 settings = Settings()
