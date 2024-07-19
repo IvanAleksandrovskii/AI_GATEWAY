a9726d50b1e7d5cdebc9937b53c3ddcd4f67f6c9
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-07-19 23:33:32 +0700

    added tag for router

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 main_app = FastAPI(lifespan=lifespan)
 
-main_app.include_router(api_router, prefix=settings.api.prefix)
+main_app.include_router(api_router, prefix=settings.api.prefix, tags=["AI"])
 
 if __name__ == "__main__":
     uvicorn.run("main:main_app", host=settings.run.host, port=settings.run.port, reload=True)
