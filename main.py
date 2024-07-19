12b3d342085459c1858b0a096633cc0f8efdbee9
Ivan Aleksandrovskii <i.aleksandrovskii@chrona.ai>
2024-07-19 11:15:45 +0700

    created lifespan for app with db dispose on shutdown

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 from fastapi import FastAPI
 import uvicorn
 
-from api import router as api_router
 from core.config import settings
 
-app = FastAPI()
+from api import router as api_router
+from core.models import db_helper
 
-app.include_router(api_router, prefix=settings.api.prefix)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # Startup
+    yield
+    # Shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+
+main_app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
+    uvicorn.run("main:main_app", host=settings.run.host, port=settings.run.port, reload=True)
